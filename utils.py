b66bcce489a6a515f2a53c247c7fd06b0ebdcf67
SPBoss69 <145862505+SPBoss69@users.noreply.github.com>
2025-04-06 16:12:28 +0400

    Update utils.py
    added comments

--- utils.py
+++ utils.py
@@ -4,31 +4,33 @@ import pytz
 class Utils:
     @staticmethod
     def get_localtime():
-        # Define the Dubai timezone
+        # Define the Dubai timezone using the pytz library
         dubai_tz = pytz.timezone("Asia/Dubai")
 
-        # Get the current time in Dubai
+        # Get the current local time in Dubai, adjusting for the timezone
         dubai_time = datetime.now(dubai_tz)
 
-        return dubai_time#.strftime("%Y-%m-%d %H:%M:%S")
+        # Return the current date and time as a datetime object
+        return dubai_time  # The full datetime object, not formatted yet
     
     @staticmethod
     def get_localdate():
-        # Define the Dubai timezone
+        # Define the Dubai timezone using the pytz library
         dubai_tz = pytz.timezone("Asia/Dubai")
 
-        # Get the current time in Dubai
+        # Get the current local time in Dubai, adjusting for the timezone
         dubai_time = datetime.now(dubai_tz)
 
-        return dubai_time.strftime("%Y-%m-%d")
+        # Return only the date part of the current time, formatted as 'YYYY-MM-DD'
+        return dubai_time.strftime("%Y-%m-%d")  # Returns a string formatted as 'YYYY-MM-DD'
     
     @staticmethod
     def get_date():
-        # Define the Dubai timezone
+        # Define the Dubai timezone using the pytz library
         dubai_tz = pytz.timezone("Asia/Dubai")
 
-        # Get the current time in Dubai
+        # Get the current local time in Dubai, adjusting for the timezone
         dubai_time = datetime.now(dubai_tz)
 
-        return dubai_time
-
+        # Return the current date and time as a datetime object
+        return dubai_time  # The full datetime object, like 'get_localtime', but no formatting
